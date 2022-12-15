d94fea6120d6b05165090c486a10e95d539ea1cf
Franklyn04 <franklinoshomegie10@gmail.com>
2022-12-15 15:46:21 -0800

    all mandatory task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagonal - printing a diagonal line
+ * @n: number of times
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n);
+{
+	int i;
+
+	for (i = '0'; i < 'n'; i++)
+	{
+		_putchar('\')
+	}
+	_putchar('\n')
+}

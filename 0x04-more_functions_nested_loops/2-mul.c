5059bd5231571098bd8f70fd9c9e6471bc4f8086
Franklyn04 <franklinoshomegie10@gmail.com>
2022-12-15 16:04:01 -0800

    all mandaotry task

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,10 +5,9 @@
  * mul - multiplication of two integers
  * @a: first integer
  * @b: second integer
- *
  * Return: addition of a and b
  */
-int mul(int a , int b)
+int mul(int a, int b)
 {
 	return (a * b);
 }
